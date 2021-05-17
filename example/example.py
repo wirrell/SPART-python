31f1a9fb5899470199b52e2efc6f7133eb00aacf
LukasValentin Graf <graflu@kp140-208.ethz.ch>
2021-05-17 10:40:11 +0200

    intermediate

--- example/example.py
+++ example/example.py
@@ -4,10 +4,10 @@ import SPART
 leafbio = SPART.LeafBiology(Cab=40, Cca=10, Cw=0.02, Cdm=0.01, Cs=0, Cant=10, N=1.5)
 
 # run soil model (BSM)
-soilpar = SPART.SoilParameters(B=0.5, lat=0, lon=100, SMp=15)
+soilpar = SPART.SoilParameters(B=0.5, lat=47, lon=8, SMp=15)
 
 # run SAILH
-canopy = SPART.CanopyStructure(LAI=3, LIDFa=-0.35, LIDFb=-0.15, q=0.05)
+canopy = SPART.CanopyStructure(LAI=3, LIDFa=-0.35, LIDFb=-0.15, q=0.05) # h = hot spot parameter
 
 # define sun-observer geometry
 angles = SPART.Angles(sol_angle=40, obs_angle=0, rel_angle=0)
@@ -16,7 +16,7 @@ angles = SPART.Angles(sol_angle=40, obs_angle=0, rel_angle=0)
 atm = SPART.AtmosphericProperties(aot550=0.3246, uo3=0.3480, uh2o=1.4116, Pa=1013.25)
 
 # run model for specific sensor (in the example, MODIS)
-spart_s2 = SPART.SPART(soilpar, leafbio, canopy, atm, angles, sensor='Sentinel2A', DOY=100)
+spart_s2 = SPART.SPART(soilpar, leafbio, canopy, atm, angles, sensor='Sentinel2A-MSI', DOY=100)
 spart_s3 = SPART.SPART(soilpar, leafbio, canopy, atm, angles, sensor='Sentinel3A-OLCI', DOY=100)
 results_s2 = spart_s2.run()  # Pandas DataFrame containing R_TOC, R_TOA, L_TOA
 results_s3 = spart_s3.run()
@@ -24,5 +24,6 @@ results_s3 = spart_s3.run()
 
 import matplotlib.pyplot as plt
 
-plt.plot(results_l8.R_TOA)
+plt.plot(results_s2.R_TOA)
+plt.show()
 
