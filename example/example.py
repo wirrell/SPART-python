e5d6fe83c7e7c13323a5a7be9a5deb2e29bad667
George Worrall <worrall.george@gmail.com>
2022-03-27 15:05:14 -0400

    Updated example to reflect code changes

--- example/example.py
+++ example/example.py
@@ -6,26 +6,28 @@ It shows to use cases:
     1) Run SPART with PROSPECT5-D as leaf model
     2) Run SPART with PROSPECT-PRO as leaf model
 """
-
-import spart as SPART
+import SPART
 
 # CASE 1: PROSPECT-D
-# run leaf model (PROSPECT-D)
+
+# Use default values from original SPART paper
+
+# Define leaf biology
 leafbio = SPART.LeafBiology(Cab=40, Cca=10, Cw=0.02, Cdm=0.01, Cs=0, Cant=10, N=1.5)
 
-# run soil model (BSM)
-soilpar = SPART.SoilParameters(B=0.5, lat=20, lon=50, SMp=15)
+# Define soil parameters
+soilpar = SPART.SoilParameters(B=0.5, lat=0, lon=100, SMp=20)
 
-# run SAILH
-canopy = SPART.CanopyStructure(LAI=3, LIDFa=-0.35, LIDFb=-0.15, q=0.05) # h = hot spot parameter
+# Define canopy structure
+canopy = SPART.CanopyStructure(LAI=3, LIDFa=-0.35, LIDFb=-0.15, q=0.05)
 
-# define sun-observer geometry
+# Define sun-observer geometry
 angles = SPART.Angles(sol_angle=40, obs_angle=0, rel_angle=0)
 
-# run atmosphere model (SMAC)
-atm = SPART.AtmosphericProperties(aot550=0.3246, uo3=0.3480, uh2o=1.4116, Pa=1013.25)
+# Define atmospheric properties
+atm = SPART.AtmosphericProperties(aot550=0.325, uo3=0.35, uh2o=1.41, Pa=1013.25)
 
-# run model for specific sensor (in the example, Sentinel2A-MSI)
+# run model for specific sensor and DOY (in the example, Sentinel2A-MSI)
 spart_s2_prospect5d = SPART.SPART(soilpar,
                                   leafbio,
                                   canopy,
@@ -34,8 +36,9 @@ spart_s2_prospect5d = SPART.SPART(soilpar,
                                   sensor='Sentinel2A-MSI',
                                   DOY=100)
 results_s2_prospect5d = spart_s2_prospect5d.run()  # Pandas DataFrame containing R_TOC, R_TOA, L_TOA
+print(results_s2_prospect5d)
 
-# CASE 2: PROSPECT-PRO (affects change in leafbio, only
+# CASE 2: PROSPECT-PRO, uses additional leaf parameters
 leafbio = SPART.LeafBiology(Cab=40, Cca=10, Cw=0.02, Cs=0, Cdm=0.0, Cant=10, N=1.5, PROT=0.0012, CBC=0.0035)
 
 spart_s2_prospectpro = SPART.SPART(soilpar,
@@ -46,3 +49,4 @@ spart_s2_prospectpro = SPART.SPART(soilpar,
                                   sensor='Sentinel2A-MSI',
                                   DOY=100)
 results_s2_prospectpro = spart_s2_prospectpro.run()  # Pandas DataFrame containing R_TOC, R_TOA, L_TOA
+print(results_s2_prospectpro)
